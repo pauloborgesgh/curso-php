ac85bac9b861d1e548fc85f3589e035ba40b81fe
borgesgh <pauloborgeszambon@gmail.com>
2026-01-19 19:54:17 -0300

    Initial commit

---
+++ exercicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./stilo/exec.css">
+  <link rel="stylesheet" href="./stilo/stilo.css">
+    <title>Coder PHP</title>
+  </head>
+<body class="exercicio">
+   <header class="cabecalho">
+    <h1>Exercicios PHP</h1>
+    <h2>Vizualização dos Exercícios</h2>
+   </header>
+      <nav class="navegacao">
+        <a href="<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?>" class="green">Sem Formatação</a>
+        <a href="index.php" class="red">Voltar</a>
+        
+      </nav>
+   <main class="principal">
+    <div class="conteudo">
+      <?php
+        include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+        
+      ?>
+   </main>
+
+   <footer class="rodape">
+    Coder PHP ©  <?= date("Y"); ?>
+   </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./stilo/stilo.css">
+    <title>Coder PHP</title>
+  </head>
+<body>
+   <header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Índice dos Exercícios</h2>
+   </header>
+
+   <main class="principal">
+    <div class="conteudo">
+      <nav class="modulos">
+        <div class="modulo white">
+          <h3>Módulo 01</h3>
+          <ul>
+            <li><a href="exercicio.php">Exercicio</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo purple">
+          <h3>Módulo 02</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo yellow">
+          <h3>Módulo 03</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo blue">
+          <h3>Módulo 04</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo orange">
+          <h3>Módulo 05</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo red">
+          <h3>Módulo 06</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo green">
+          <h3>Módulo 07</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo pink">
+          <h3>Módulo 08</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+        <div class="modulo black">
+          <h3>Módulo 09</h3>
+          <ul>
+            <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+            <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+            <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+            <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
+            
+          </ul>
+        </div>
+
+      </nav>
+   </main>
+
+   <footer class="rodape">
+    Coder PHP ©  <?= date("Y"); ?>
+   </footer>
+</body>
+</html>
